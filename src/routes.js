e85a39570ab38a2e7281de9778d5a9b29b1cabfd
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-04-12 21:44:10 -0300

    Eslint config

--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,10 @@ routes.post('/boxes', BoxController.store)
 routes.get('/boxes/:id', BoxController.show)
 
 routes.post(
-    '/boxes/:id/files',
-    multer(multerConfig).single('file'),
-    FileController.store
+	'/boxes/:id/files',
+	multer(multerConfig).single('file'),
+	FileController.store
 )
 
 module.exports = routes
+
--- src/server.js
+++ src/server.js
@@ -10,23 +10,23 @@ app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-io.on('connection', socket => {
-    socket.on('connectRoom', box => {
-        socket.join(box)
-    })
+io.on('connection', (socket) => {
+	socket.on('connectRoom', (box) => {
+		socket.join(box)
+	})
 })
 
 mongoose.connect('mongodb+srv://antoninhom:a09020206@cluster0-togdg.mongodb.net/omnistack?retryWrites=true', {
-    useNewUrlParser: true
+	useNewUrlParser: true,
 })
 
 app.use((req, res, next) => {
-    req.io = io
-    return next()
+	req.io = io
+	return next()
 })
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
